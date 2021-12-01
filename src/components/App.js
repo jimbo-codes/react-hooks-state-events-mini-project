343d305a3da34ac47c3ce7ddf61c5154b0ad742b
James Burns <jrburns96@aol.com>
2021-12-01 15:43:58 -0500

    Fixed em all

--- src/components/App.js
+++ src/components/App.js
@@ -3,45 +3,32 @@ import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 import {useState} from 'react';
-
 import { CATEGORIES, TASKS } from "../data";
-// console.log("Here's the data you're working with");
-// console.log({ CATEGORIES, TASKS });
 
 function App() {
   const [objList, setObjList] = useState(TASKS);
   const [mySelected, setButton] = useState('')
-  const [name, setName]  = useState('');
-  const [categorySelected, setCategory] = useState('Code');
-  // const [newTask]
+
 
   function filterApp(event){
     setButton(event.target.textContent);
-    console.log(objList);
     setObjList(TASKS.filter((obj) => {
         if(event.target.textContent==="All"){
-        return objList;
-      }else {
-        return  obj.category===event.target.textContent
-      }
+          return objList;
+        }else {
+          return  obj.category===event.target.textContent
+        }
     }))
   }
-  function nameHandler(event){
-    setName(event.target.value)
-  }
-  function categoryHandler(event){
-    // console.log(event.target.value);
-    setCategory(event.target.value)
-  }
+
   
-  function handleSubmit(event){
-    event.preventDefault();
-    console.log(name)
-    const newTask={
-      text: name,
-      category: categorySelected,
-    }
-    const newObject2 = [...objList,newTask]
+  function handleSubmit(object){
+    // event.preventDefault();
+    // const newTask={
+    //   text: name,
+    //   category: categorySelected,
+    // }
+    const newObject2 = [...objList,object]
     // modify the array being used to render to add on the new thing.
    setObjList(newObject2)
    console.log(objList)
@@ -56,10 +43,10 @@ function App() {
       />
       <NewTaskForm 
       categories={CATEGORIES.slice(1)}
-      categoryCapture={categoryHandler}
-      nameCapture={nameHandler}
-      categorySelected={categorySelected}
-      nameSelected={name}
+      // categoryCapture={categoryHandler}
+      // nameCapture={nameHandler}
+      // categorySelected={categorySelected}
+      // nameSelected={name}
       onTaskFormSubmit = {handleSubmit}
       />
       <TaskList 
